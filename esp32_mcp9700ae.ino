67b05b16ed4091eddb89eaa2abc87251607d1023
msyk9038 <msyk9038@gmail.com>
2023-01-08 15:24:53 +0900

    アナログ温度センサmcp9700ae 用のスケッチを追加。LM35とピン配置は変わらないので、回路はLM35と同じでOK。500mV@0℃なので、その部分だけ変更。

---
+++ esp32_mcp9700ae.ino
@@ -0,0 +1,21 @@
+const float Vcc = 3.3;
+const int ANALOG_MAX = 4096;
+const int analogPin = 34;
+const int delayTime = 1000;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+void loop() {
+  int reading = analogRead(analogPin);
+  float voltage = ((long)reading * Vcc * 1000) / ANALOG_MAX;
+  float temp = (voltage - 500) / 10; // +10mV/degree_Celsius
+  Serial.print(temp);
+  Serial.println(" degree_Celsius.");
+  delay(delayTime);
+}
