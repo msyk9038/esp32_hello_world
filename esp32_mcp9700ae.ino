0582b24eb4f6d51c3aa7ac95c3ad08854f6e80bf
msyk9038 <msyk9038@gmail.com>
2023-01-08 16:15:02 +0900

    アナログ温度センサmcp9700ae 用のスケッチを追加。シリアルプロッタでグラフを表示できるように改造。

--- esp32_mcp9700ae.ino
+++ esp32_mcp9700ae.ino
@@ -2,6 +2,32 @@ const float Vcc = 3.3;
 const int ANALOG_MAX = 4096;
 const int analogPin = 34;
 const int delayTime = 1000;
+const int stock_rotation_term = 10;
+float stock_data[stock_rotation_term];
+int stock_rotation_number = 0;
+bool enough_data_flag = false;
+
+float moving_average(float data) {
+  stock_rotation_number ++;
+  if (stock_rotation_number >= stock_rotation_term) {
+    stock_rotation_number = 0;
+    enough_data_flag = true;
+  }
+  stock_data[stock_rotation_number] = data;
+  float result = 0;
+  for (int i=0; i < stock_rotation_term; i++) {
+    result += stock_data[i];
+  }
+  result = result / stock_rotation_term;
+  
+  if (enough_data_flag) {
+    // Serial.print("moving ave: ");
+    return result;
+  } else {
+    // Serial.print("real data: ");
+    return data;
+  }
+}
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -15,7 +41,8 @@ void loop() {
   int reading = analogRead(analogPin);
   float voltage = ((long)reading * Vcc * 1000) / ANALOG_MAX;
   float temp = (voltage - 500) / 10; // +10mV/degree_Celsius
-  Serial.print(temp);
-  Serial.println(" degree_Celsius.");
+  temp = moving_average(temp);
+  Serial.println(temp);
+  // Serial.println(" degree_Celsius.");
   delay(delayTime);
 }
